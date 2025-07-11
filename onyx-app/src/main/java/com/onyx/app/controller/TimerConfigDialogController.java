8d0e448fccb3f6aa9854c87999602e8d468054d5
neocrafting <100075595+Jcod007@users.noreply.github.com>
2025-07-10 22:03:42 -0400

    Ajout du module pour la configuration des timers

---
+++ onyx-app/src/main/java/com/onyx/app/controller/TimerConfigDialogController.java
@@ -0,0 +1,17 @@
+package com.onyx.app.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class TimerConfigDialogController {
+
+	@FXML
+	private TextField timerTextFliedConfig;
+	
+	public void initialize()
+	{
+		
+	}
+	
+	
+}
